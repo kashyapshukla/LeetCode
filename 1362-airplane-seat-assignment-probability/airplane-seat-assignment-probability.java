49e841b488485a2ff1fe8eab41d8f4ead147e6ed
Kashyap Shukla <66747576+kashyapshukla@users.noreply.github.com>
2024-11-22 16:58:35 -0500

    Time: 23 ms (7.63%) | Memory: 50.7 MB (5.19%) - LeetSync

--- 1362-airplane-seat-assignment-probability/airplane-seat-assignment-probability.java
+++ 1362-airplane-seat-assignment-probability/airplane-seat-assignment-probability.java
@@ -3,7 +3,11 @@ class Solution {
 
         if(n==1) return 1;
 
-        return 0.5;
+        double break_cycle=1.0/n;
+
+        double continue_cycle= ((n-2.0)/n) ;
+
+        return break_cycle + continue_cycle * nthPersonGetsNthSeat(n-1) ;
         
     }
 }
