a6e3596277bf3907733f0e2ac0260d3180cbad3a
Kashyap Shukla <66747576+kashyapshukla@users.noreply.github.com>
2024-11-30 21:36:53 -0500

    Time: 0 ms (100.00%) | Memory: 41.7 MB (7.74%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,34 +1,35 @@
 class Solution {
     public int rob(int[] nums) {
 
-        if(nums.length==1){
-            return nums[0];
-        }
+        int start=0;
 
-        if(nums.length==2){
-            return Math.max(nums[0],nums[1]);
-        }
+        if(nums.length==0)return 0;
+        if(nums.length==1)return nums[0];
+        if(nums.length==2)return Math.max(nums[0],nums[1]);
+        if(nums.length==3) return Math.max(nums[0], Math.max(nums[1], nums[2]));
+        
 
-        if(nums.length==3){
-            return Math.max(nums[0],Math.max(nums[1],nums[2]));
-        }
+        return Math.max(helper(nums, 0,nums.length-2), helper(nums,1,nums.length-1));
 
+        
 
-        return Math.max(helper(nums,0, nums.length-2), helper(nums,1,nums.length-1));      
     }
 
+    public int helper(int[] nums,int start, int end){
+          
+          int r1=0;
+          int r2=0;
+          for(int i=start;i<=end;i++){
 
-    int helper(int nums[], int start, int end){
-        int r1=0;
-        int r2=0;
+            int temp=Math.max(r1+nums[i], r2);
+            r1=r2;
+            r2=temp;
 
-        for(int i=start;i<=end;i++){
-            int tem=Math.max(r1, nums[i]+r2);
-            r2=r1;
-            r1=tem;
-        }
+          }
 
-
-        return r1;
+        
+    return r2;
     }
+
+   
 }
